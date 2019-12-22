eb73abd5c87ff634dcb9ade0512fb78c3bd36b96
ShouDong Zheng <admin@zhengshoudong.cn>
2019-12-22 15:13:16 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+var (
+	_host          string
+	_port          int
+	_password      string
+	_clusterClient *redis.ClusterClient
+)
+
+func main() {
+	flag.StringVar(&_host, "host", "", "Host")
+	flag.IntVar(&_port, "port", 0, "Port")
+	flag.StringVar(&_password, "password", "", "Password")
+
+	if len(_host) == 0 || _port == 0 {
+		flag.Usage()
+		return
+	}
+
+	initClusterClient([]string{getAddr(_host, _port)}, _password)
+	defer _clusterClient.Close()
+
+	slaveClients := getAllSlaveNodes()
+
+	for _, client := range slaveClients {
+		fmt.Println("------------------" + client.GetAddr() + "------------------")
+		waitBgsaveFinish(client)
+		if bgsaveSuccess, err := bgsave(client); err != nil || !bgsaveSuccess {
+			fmt.Println("Background saving failed.", err)
+			os.Exit(-1)
+		}
+
+		fmt.Println("Background saving started.")
+		waitBgsaveFinish(client)
+		time.Sleep(time.Second * 5)
+	}
+
+	fmt.Println("------------------------------------")
+	fmt.Println(strconv.Itoa(len(slaveClients)) + " nodes are completed!")
+}
+
+func getAllSlaveNodes() []*redis.Client {
+	clients := []*redis.Client{}
+
+	_clusterClient.ForEachSlaveSync(func(client *redis.Client) error {
+		clients = append(clients, client)
+		return nil
+	})
+
+	if len(clients) == 0 {
+		fmt.Println("No slave nodes!")
+		os.Exit(-1)
+	}
+
+	return clients
+}
+
+func waitBgsaveFinish(client *redis.Client) {
+	inProgress, err := getBgsaveInProgress(client)
+	if err != nil {
+		panic(err)
+	}
+
+	if !inProgress {
+		return
+	}
+
+	for {
+		time.Sleep(time.Second * 5)
+		fmt.Println("check status...")
+		inProgress, err = getBgsaveInProgress(client)
+		if !inProgress {
+			fmt.Println("finished!")
+			break
+		}
+	}
+}
+
+func getBgsaveInProgress(client *redis.Client) (bool, error) {
+	info, err := client.Info("Persistence").Result()
+	if err != nil {
+		return false, err
+	}
+
+	tag := "rdb_bgsave_in_progress:"
+	startIndex := strings.Index(info, tag) + len(tag)
+	return info[startIndex:startIndex+1] == "1", nil
+}
+
+func bgsave(client *redis.Client) (bool, error) {
+	result, err := client.BgSave().Result()
+	if err != nil {
+		return false, err
+	}
+
+	return result == "Background saving started", nil
+}
+
+func initClusterClient(addrs []string, password string) {
+	fmt.Println("Cluster client initing..")
+
+	_clusterClient = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs:        addrs,
+		Password:     password,
+		PoolSize:     32,
+		MinIdleConns: 6,
+	})
+
+	pong, err := _clusterClient.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+
+	if pong != "PONG" {
+		fmt.Println(pong)
+		os.Exit(-1)
+	}
+}
+
+func getAddr(host string, port int) string {
+	return host + ":" + strconv.Itoa(port)
+}
